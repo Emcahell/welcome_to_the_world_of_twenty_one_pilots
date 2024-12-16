fa7c5ad09965ec28fad74823aba6fff7b2474347
emcahell <emmanuelltowers@gmail.com>
2024-12-15 20:34:34 -0400

    arregle el pinche modal

--- js/modal-cards.js
+++ js/modal-cards.js
@@ -5,10 +5,11 @@ btnsOpen.forEach(btn => {
     btn.addEventListener('click', () => {
         const modalId = btn.getAttribute('data-modal');
         const tracklist = document.getElementById(modalId);
-        console.log(modalId);
-        console.log('click!');
-                
         tracklist.classList.add('active');
+        
+        const element = document.querySelector('.box-card-albums');
+        const styleSheet = document.styleSheets[1];
+        styleSheet.insertRule('.box-card-albums::before{display: block; content: ""; position: absolute; top: 0; left: 0; width: 100vw; height: 100%; background-color: rgba(17, 17, 17, 0.473); backdrop-filter: blur(8px);}', styleSheet.cssRules.length);
     });
 });
 
@@ -21,8 +22,10 @@ btnsClosed.forEach(btn => {
 const closeModal = () => {
     const modals = document.querySelectorAll('.tracklist');
     modals.forEach(modal => {
-        console.log('closed');
-        
         modal.classList.remove('active');
+    
+        const element = document.querySelector('.box-card-albums');
+        const styleSheet = document.styleSheets[1];
+        styleSheet.insertRule('.box-card-albums::before{display: none; content: ""; position: absolute; top: 0; left: 0; width: 100vw; height: 100%; background-color: rgba(17, 17, 17, 0.473); backdrop-filter: blur(8px);}', styleSheet.cssRules.length);
     });
 }
